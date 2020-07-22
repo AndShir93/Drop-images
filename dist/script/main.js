ff69234c96117f5819fca4897571583a591bfebb
Andrey <andshir93@gmail.com>
2020-07-23 01:30:10 +0400

    bug fixed

--- dist/script/main.js
+++ dist/script/main.js
@@ -3,13 +3,12 @@ const filesItem = document.querySelectorAll('.files-wrapper__item');
 const button = document.querySelector('#drop');
 let arr = [];
 
-let insertImages = function(){
+let insertImages = function(t){
     for(let i = 0; i < arr.length; i++){
         filesItem[i].innerHTML = arr[i];
-        let a = arr.length > 15 ? button.setAttribute('disabled', true) : button.removeAttribute('disabled');
+        let a = arr.length > t ? button.setAttribute('disabled', true) : button.removeAttribute('disabled');
     };
 }
-
 
 if(localStorage.getItem('images')){
     arr = JSON.parse(localStorage.getItem('images'));
@@ -18,9 +17,9 @@ if(localStorage.getItem('images')){
 
 
 
-let openFile = function() {
+let openFile = function(p) {
     let files = button.files;
-    for(let i = 0; i < files.length; i++){
+    for(let i = 0; i < p; i++){
         let reader = new FileReader();
         reader.readAsDataURL(files[i]);
         reader.onload = function(){
@@ -28,13 +27,20 @@ let openFile = function() {
             let el = `<img class="files-wrapper__images" src=${urlFiles}>`;
             arr.unshift(el);
             localStorage.setItem('images', JSON.stringify(arr));
-            insertImages();
+            insertImages(p);
         }
     }
 };
 
 button.onchange = function(){
-    openFile();
+    let files = button.files;
+    let count;
+    if(files.length + arr.length > 16){
+        count = 16 - arr.length;
+    }else{
+        count = files.length;
+    }
+    openFile(count);
 };
 filesWrapper.ondrop = function(event){
     button.change;
