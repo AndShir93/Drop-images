1412cbe455202eac9dde47f466d30500f38bc237
Andrey <andshir93@gmail.com>
2020-07-22 09:31:38 +0400

    save images in localStorage

--- dist/script/main.js
+++ dist/script/main.js
@@ -1,6 +1,16 @@
-let files = document.querySelectorAll('.files-wrapper__item');
+const filesWrapper = document.querySelector('.drop-wrapper');
+const files = document.querySelectorAll('.files-wrapper__item');
 const button = document.querySelector('#drop');
 let arr = [];
+
+// filesWrapper.addEventListener('drag',function(event){event.preventDefault()});
+// filesWrapper.addEventListener('dragstart',function(event){event.preventDefault()});
+// filesWrapper.addEventListener('dragend',function(event){event.preventDefault()});
+// filesWrapper.addEventListener('dragover',function(event){event.preventDefault()});
+// filesWrapper.addEventListener('dragenter',function(event){event.preventDefault()});
+// filesWrapper.addEventListener('dragleave',function(event){event.preventDefault()});
+// filesWrapper.addEventListener('drop',function(event){event.preventDefault()});
+
 
 if(localStorage.getItem('images')){
     arr = JSON.parse(localStorage.getItem('images'));
@@ -12,19 +22,21 @@ if(localStorage.getItem('images')){
 let openFile = function(event) {
 let input = event.target;
 let reader = new FileReader();
-
+console.log(reader);
     reader.onload = function(){
-        console.log();
         let dataURL = reader.result;
-        let map = dataURL;
-        let el = `<img class="files-wrapper__images" src=${map}>`;
+        let el = `<img class="files-wrapper__images" src=${dataURL}>`;
         arr.unshift(el);
         localStorage.setItem('images', JSON.stringify(arr));
         arr.forEach(function(item, i, arr){
             files[i].innerHTML = arr[i];
         });
+        reader = '';
         let a = arr.length === 16 ? button.setAttribute('disabled', true) : button.removeAttribute('disabled');
     };    
 reader.readAsDataURL(input.files[0]);
 };
-button.onchange = function(){openFile(event);}
+button.onchange = function(){openFile(event)};
+filesWrapper.ondrop = function(){
+    openFile(event);
+}
